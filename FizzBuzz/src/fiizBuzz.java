45516cb3c4e9e985409259d66e8d399a0c401c17
sarah.evans <sarah.evans@MU-C1-01.mullen.pvt.k12.co.us>
2019-08-28 11:44:58 -0600

    initial and final commit

---
+++ FizzBuzz/src/fiizBuzz.java
@@ -0,0 +1,31 @@
+
+public class fiizBuzz
+{
+
+	public static void main(String[] args)
+	{
+		int i = 1;
+		for ( i = 1; i < 101; i ++ )
+		{
+			if (i % 3 == 0 && i % 5 == 0)
+			{
+				System.out.println("FizzBuzz");
+			}
+			
+			else if (i % 5 == 0)
+			{
+				System.out.println("Buzz");
+			}
+			
+			else if (i % 3 == 0)
+			{
+				System.out.println("Fizz");
+			}
+			else
+			{
+			System.out.println(i);
+			}
+		}
+	}
+
+}
